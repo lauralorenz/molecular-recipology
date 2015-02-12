ffcc793b35f5e22c626a056252ecff68a814fdb0
Laura <lalorenz6@gmail.com>
2015-02-12 14:43:15 -0500

    Clean density field during serialization.

--- recipe/scaler/serializers.py
+++ recipe/scaler/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from models import Compounds, Foods
+import re
 
 
 class FoodsSerializer(serializers.ModelSerializer):
@@ -9,9 +10,13 @@ class FoodsSerializer(serializers.ModelSerializer):
 
 
 class CompoundsSerializer(serializers.ModelSerializer):
+    density = serializers.SerializerMethodField('prepare_density')
     class Meta:
         model = Compounds
         fields = ['id', 'moldb_average_mass', 'density']
+
+    def prepare_density(self, obj):
+        return re.search('(?<= )(?:\d\.)?\d+', obj.density).group(0)
 
 
 class CompoundsFoodsSerializer(serializers.ModelSerializer):
